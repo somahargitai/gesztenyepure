59971a60d7f91fb6e3f3d3da8daa1de3396c1337
Soma Hargitai <hargitai.soma@gmail.com>
2023-08-30 18:25:13 +0200

    vite config refresh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/gesztenyepure/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
   server: {
     headers: {
