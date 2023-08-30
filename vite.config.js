02843073306da661e576449837dedcd6eb78f36e
Soma Hargitai <hargitai.soma@gmail.com>
2023-08-30 15:13:23 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
-})
+  server: {
+    headers: {
+      'Permissions-Policy': 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+    }
+  }
+});
